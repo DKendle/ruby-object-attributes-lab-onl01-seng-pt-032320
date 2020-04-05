d5bd27a598bbeec23036eee2aa3729e9520bbc1b
Dina Kendle <dina.kendle@gmail.com>
2020-04-05 14:19:12 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  def name=(dog_name)
+    @this_name = dog_name
+  end
+  
+  def name
+    @this_name
+  end
+  
+  def breed=(dog_breed)
+    @this_breed = dog_breed
+  end
+  
+  def breed
+    @this_breed
+  end
+end
+
+fido = Dog.new 
+fido.name = "Fido"
+fido.name 
